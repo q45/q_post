b2823ccf95e40d2e482c9235781a90739ba07706
q45 <quintinsmith@icloud.com>
2013-09-20 17:14:50 -0600

    add comment to user model backed form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
 
-def show
-	@comment = Comment.new
+def create
+	@post = Post.find(params[:post_id]) 
+	#@comment = Comment.new(comment_params)
+	#@comment.post = @post # these two are the same as line 8
+
+	@comment = @post.comments.build(comment_params) #build is the same thing as new
+
+	@comment.user_id = 1
+
+	if @comment.save
+		flash[:notice] = "Your comment was saved"
+		redirect_to post_path(@post)
+	else
+		render 'posts/show'
+	end
+
+	#I need to associate this comment with a post. This is how to do it.
+end
+
+private
+
+def comment_params
+	params.require(:comment).permit(:body)
 end
 
 end
