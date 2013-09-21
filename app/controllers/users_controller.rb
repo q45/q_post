96bbbb4dc691a749f9679d56a779b211bc991b29
q45 <quintinsmith@icloud.com>
2013-09-21 13:58:42 -0600

    working authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
 
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+				flash[:notice] = "You are registered"
+		else
+			redirect_to root_path
+		end
+	end
+
 	def show
 		@user = User.first
 		
 	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:username, :password)
+		end
 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 PostitTemplate::Application.routes.draw do
+	root to: 'posts#index'
+
+	get '/login', to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	get '/logout', to: 'sessions#destroy'
+
   resources :posts, except: :destroy do
   	resource :comments #:only => :create
   	#/posts/2/comments 
   end
   resources :categories, only: [:new, :create, :show]
   resources :users
-  root to: 'posts#index'
+
 
 end
